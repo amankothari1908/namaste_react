5476985798cfc8a43e891aac54c30274d39d94aa
Aman <aman@Amans-MacBook-Air.local>
2024-08-31 10:29:42 +0530

    lec done

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {}, "HEELO PARCEL ");
-const heading2 = React.createElement("h2", {}, "NICE GOING ON");
-const container = React.createElement("div", {}, [heading1, heading2]);
+// Js Function
+const Heading = () => (
+  <h1 id="title" key="h1">
+    THIS IS HEADING
+  </h1>
+);
+
+// React Component
+const HeaderComponent = () => {
+  return (
+    <div>
+      {Heading()} or <Heading /> (this is called component composition)
+      <h1>This is amazing</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent />);
